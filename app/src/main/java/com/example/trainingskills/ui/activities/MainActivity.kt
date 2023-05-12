627a5b941b75bcb2a02a05059b86d4886aeca924
dangaliu <ii4603399@gmail.com>
2023-05-12 12:54:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/trainingskills/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trainingskills.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trainingskills.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/trainingskills/ui/fragments/SplashFragment.kt
@@ -0,0 +1,22 @@
+package com.example.trainingskills.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trainingskills.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
